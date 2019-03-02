9a94dfbad4ff17e3f6e14d411c5fcc6c7158f330
Erin Alltop <erin.alltop@gmail.com>
2019-03-02 15:35:02 -0500

    finished

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,9 +1,23 @@
 # Day 2 of Advent of Code 2018
 # Inventory Management System
-# What is the cheksum of the list of box IDs?
+# What is the checksum of the list of box IDs?
 
-with open('input.txt') as input:
-    boxIDs = input.read()
-input.closed
+from collections import Counter
 
-print(boxIDs)
+data = open('input.txt', 'r')
+boxIDs = data.read().strip().splitlines()
+data.close()
+
+# tracking counts
+count = [0, 0]
+
+for i in boxIDs:
+    search = [j for i, j in Counter(i).most_common()]
+    if 3 in search:
+        count[0] += 1
+    if 2 in search:
+        count[1] += 1
+count
+
+solution = (count[0] * count[1])
+print (solution)
